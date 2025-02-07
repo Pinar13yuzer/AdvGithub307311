5849799029792116918ca8b34cda02900305e3df
Pınar <Pyuzer13@gmail.com>
2025-02-07 20:50:20 +0300

    version 2

--- src/MyClass.java
+++ src/MyClass.java
@@ -6,6 +6,9 @@ public class MyClass {
         //degisiklik 1
         System.out.println("Herkese selamlar");
 
+        //degisiklik 2
+        System.out.println("Adv Github dersine hosgeldiniz");
+
 
 
     }
